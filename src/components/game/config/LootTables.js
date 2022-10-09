98113646187eecd2c98b1bfff5fcca7844ff2f6c
Ldalvik <subaruforge@gmail.com>
2022-10-09 17:25:51 -0400

    Added constants file for boat loot tables

---
+++ src/components/game/config/LootTables.js
@@ -0,0 +1,53 @@
+export const rentalBoatLootTable =  [
+    {
+        name: "Carp",
+        weight: 100,
+        price: 1,
+        accumulatedWeight: null
+    },
+    {
+        name: "Cod",
+        weight: 100,
+        price: 1,
+        accumulatedWeight: null
+    },
+    {
+        name: "Sunfish",
+        weight: 90,
+        price: 2,
+        accumulatedWeight: null
+    },
+    {
+        name: "Trout",
+        weight: 80,
+        price: 3,
+        accumulatedWeight: null
+    }
+]
+
+export const starterBoatLootTable =  [
+    {
+        name: "Carp",
+        weight: 100,
+        price: 1,
+        accumulatedWeight: null
+    },
+    {
+        name: "Cod",
+        weight: 100,
+        price: 1,
+        accumulatedWeight: null
+    },
+    {
+        name: "Sunfish",
+        weight: 90,
+        price: 2,
+        accumulatedWeight: null
+    },
+    {
+        name: "Trout",
+        weight: 80,
+        price: 3,
+        accumulatedWeight: null
+    }
+]
