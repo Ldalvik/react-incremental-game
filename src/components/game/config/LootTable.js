4739c3a674085eb4480c453e00bfbf57cb90f830
Ldalvik <subaruforge@gmail.com>
2022-10-09 16:37:25 -0400

    Create loot generator

--- src/components/game/config/LootTable.js
+++ src/components/game/config/LootTable.js
@@ -1,48 +1,4 @@
-export const getItem = () => {
-    let entries = [
-        {
-            name: "Carp",
-            weight: 100,
-            price: 2,
-            accumulatedWeight: null
-        },
-        {
-            name: "Cod",
-            weight: 100,
-            price: 2,
-            accumulatedWeight: null
-        },
-        {
-            name: "Sunfish",
-            weight: 90,
-            price: 4,
-            accumulatedWeight: null
-        },
-        {
-            name: "Trout",
-            weight: 75,
-            price: 6,
-            accumulatedWeight: null
-        },
-        {
-            name: "Smallmouth Bass",
-            weight: 75,
-            price: 6,
-            accumulatedWeight: null
-        },
-        {
-            name: "Largemouth Bass",
-            weight: 40,
-            price: 7,
-            accumulatedWeight: null
-        },
-        {
-            name: "Catfish",
-            weight: 30,
-            price: 9,
-            accumulatedWeight: null
-        },
-    ]
+export const getItem = (entries) => {
     let count = 0
     const accumulatedWeight = entries.reduce((accumulator, item) => {
         entries[count].accumulatedWeight = accumulator + item.weight;
@@ -53,4 +9,12 @@ export const getItem = () => {
     return entries.find(function (entry) {
         return entry.accumulatedWeight >= r;
     })
+}
+
+export const getChance = (entries, itemName) => {
+    const accumulatedWeight = entries.reduce((accumulator, item) => {
+        return accumulator + item.weight;
+    }, 0)
+    const weight = entries.find(item => item.name === itemName).weight
+    return weight / accumulatedWeight
 }
